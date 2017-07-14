e71b00d90a5ed8266897bf428694b8438f8c47d2
Meracing <renecornella@gmail.com>
2017-07-14 09:34:39 +0200

    changed comments

--- src/com/rene/Main.java
+++ src/com/rene/Main.java
@@ -8,15 +8,15 @@ public class Main {
         Engine engine = new Engine("V8", "Honda", engineDetail);
 
         Car car = new Car(body,engine);
-      //  car.getEngine().engineDetail2(4,1200,90);     // only working if getter in Car are Public
-      //  car.getBody().unlockcar(" unlocked");         // only working if getter in Car are Public
+      //  car.getEngine().engineDetail2(4,1200,90);     // only working if getter in Car is Public
+      //  car.getBody().unlockcar(" unlocked");         // only working if getter in Car is Public
 
         engine.engineDetail2(4,1200,90);
         car.start();
         System.out.println(engineDetail.getWeight()+ " " + engineDetail.getWeightPowerRatio());
         System.out.println(engine.getPower().getWeight());
         System.out.println(body.getModel()+ " " +body.getColor());
-        
+
 
     }
 }
